67ccb243f453d2ca6e28d61169760d69e91240ef
SeoyoungHwang <dev.seoyoung.hwang@gmail.com>
2022-07-08 11:19:56 -0600

    add jsx in part2.jsx

--- src/components/part2.jsx
+++ src/components/part2.jsx
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
 
 class Part2 extends Component {
+
+    addPhoto = () => {
+        
+    }
+
+    photoId = React.createRef();
+    title = React.createRef();
+
     render() {
         return (
-            <div>
-                
-            </div>
+            <section>
+                <form onSubmit={this.addPhoto}>
+                    <input ref = {this.photoId} type='text' name='photoId'/>
+                    <input ref = {this.title} type='text' name='title'/>
+                    <button type='submit'>Add Photo</button>
+                </form>
+            </section>
         );
     }
 }
