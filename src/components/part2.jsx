1fa41aa92bf0f0a81b7953b7bf66752656d5b66f
SeoyoungHwang <dev.seoyoung.hwang@gmail.com>
2022-07-08 14:36:05 -0600

    fetch json api

--- src/components/part2.jsx
+++ src/components/part2.jsx
@@ -1,25 +1,74 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Part2 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      err: "",
+      photos: {
+        photoId: "",
+        title: "",
+      },
+    };
+  }
 
-    addPhoto = () => {
-        
-    }
+  addPhoto = (e) => {
+    e.preventDefault();
+    const photoId = this.photoId.current.value;
+    const title = this.title.current.value;
+    const photos = { photoId, title };
 
-    photoId = React.createRef();
-    title = React.createRef();
+    fetch(`https://jsonplaceholder.typicode.com/photos`, {
+      method: "POST",
+      body: JSON.stringify({
+        albumId: 1,
+        id: parseInt(photos.photoId),
+        title: photos.title,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((json) => {
+        this.setState({ photos });
+        console.log(json);
+      })
+      .catch((err) => {
+        console.log(err.toString());
+        this.setState({ err });
+      });
+  };
 
-    render() {
-        return (
-            <section>
-                <form onSubmit={this.addPhoto}>
-                    <input ref = {this.photoId} type='text' name='photoId'/>
-                    <input ref = {this.title} type='text' name='title'/>
-                    <button type='submit'>Add Photo</button>
-                </form>
-            </section>
-        );
-    }
+  photoId = React.createRef();
+  title = React.createRef();
+
+  render() {
+    return (
+      <section className="grid">
+        <form onSubmit={this.addPhoto}>
+          <input
+            ref={this.photoId}
+            type="text"
+            name="photoId"
+            placeholder="photo id"
+          />
+          <input
+            ref={this.title}
+            type="text"
+            name="title"
+            placeholder="title of the photo"
+          />
+          <button type="submit">Add Photo</button>
+        </form>
+
+        <p>{this.state.err ? "This photo not able to create" : ""}</p>
+      </section>
+    );
+  }
 }
 
 export default Part2;
