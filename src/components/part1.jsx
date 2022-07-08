c5ce106c711f16c982ded3cea68d14f270573a4f
SeoyoungHwang <dev.seoyoung.hwang@gmail.com>
2022-07-08 14:43:50 -0600

    complete part1.jsx

--- src/components/part1.jsx
+++ src/components/part1.jsx
@@ -1,67 +1,70 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 class Part1 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      photos: [],
+    };
+  }
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            photos: [],
-        }
-    }
-
-    componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/photos').then(res => {
-            this.setState({photos: res.data})
-        })
-    }
-
-    deleteRow = (e)=> {
-
-        axios.delete(`https://jsonplaceholder.typicode.com/photos/${e.target.value}`)
-    .then(() => {
-        const photos = this.state.photos.filter(item=>{
-            return item.id!== parseInt(e.target.value);
-        });
-        this.setState({photos });
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/photos").then((res) => {
+      this.setState({ photos: res.data });
     });
+  }
 
-    
-    }
+  deleteRow = (e) => {
+    //     axios.delete(`https://jsonplaceholder.typicode.com/photos/${e.target.value}`)
+    // .then(() => {
+    //     const photos = this.state.photos.filter(item=>{
+    //         return item.id!== parseInt(e.target.value);
+    //     });
+    // });
+    fetch(
+      `https://jsonplaceholder.typicode.com/photos/${parseInt(e.target.value)}`,
+      {
+        method: "DELETE",
+      }
+    )
+      .then((res) => res.json())
+      .then((result) => {
+        const photos = this.state.photos.filter((item) => {
+          return item.id !== parseInt(e.target.value);
+        });
 
+        this.setState({ photos });
+      })
+      .catch((err) => console.log(err.toString()));
+  };
 
-
-    render() {
-        const {photos} = this.state;
-        return (
-            <React.Fragment>
-                <h1>Photos Table</h1>
-                <table>
-                    <tbody>
-                    {
-                        photos.map(photo =>
-                            <tr key={photo.id}>
-                                <td>
-                                    {photo.id}
-                                </td>
-                                <td>
-                                    {photo.title}
-                                </td>
-                                <td>
-                                    <img src={photo.thumbnailUrl} alt='thumnail'/>
-                                </td>
-                                <td>
-                                    <button value={photo.id} onClick={this.deleteRow}>Delete</button>
-                                </td>
-                            </tr>
-                            )
-                    }
-                    </tbody>
-                </table>
-                
-            </React.Fragment>
-        );
-    }
+  render() {
+    const { photos } = this.state;
+    return (
+      <React.Fragment>
+        <h1>Photos Table</h1>
+        <table>
+          <tbody>
+            {photos.map((photo) => (
+              <tr key={photo.id}>
+                <td>{photo.id}</td>
+                <td>{photo.title}</td>
+                <td>
+                  <img src={photo.thumbnailUrl} alt="thumnail" />
+                </td>
+                <td>
+                  <button value={photo.id} onClick={this.deleteRow}>
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Part1;
